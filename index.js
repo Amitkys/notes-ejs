3de57c8417833f1ebdb9b341324668dc2db3f37e
KYs <amitkys59@gmail.com>
2024-09-22 18:17:50 +0530

    created routes for different operation

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const path = require('path');
 const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Note = require('./db/db');
 
 const app = express();
 
@@ -21,10 +23,36 @@ app.use(express.static(path.join(__dirname, './public')));
 app.use(methodOverride('_method'));
 // ........................................................................
 
+mongoose.connect('mongodb://localhost:27017/note')
+.then(() => {console.log('connected to db')}).catch((e) => console.log('error to connect db', e));
 
 app.get('/', (req, res) => {
     res.render('routes/index.ejs');
 })
+
+app.post('/note', async(req, res) => {
+    const newNote = {title: req.body.title, note: req.body.note};
+    const createdNote = await Notes.create(newNote);
+    // console.log(createdNote);
+    res.json({msg: 'notes created', noteId: createdNote._id});
+})
+app.put('/note/:id', async(req, res) => {
+    const id = req.params.id;
+    const newNote = {title: req.body.title, note: req.body.note};
+    const updatedNote = await Notes.findByIdAndUpdate(id, newNote);
+    res.json({msg: 'Note Updated'});
+})
+app.get("/note", async(req, res) => {
+    const all_note = await Notes.find({});
+    res.json({all_note});
+});
+app.delete("/note/:id", async(req, res) => {
+    const id = req.params.id;
+    const deletedNote = await Notes.findByIdAndDelete(id);
+    res.json({msg: 'notes delete'});
+
+})
+
 
 
 app.listen(3000, () => console.log('listening on 3000'));
