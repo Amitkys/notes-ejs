5cd241ea99125c423dc3c1d91fb6162e11a0066b
KYs <amitkys59@gmail.com>
2024-09-22 17:52:59 +0530

    Intialize project with required file

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const path = require('path');
+const ejsMate = require('ejs-mate');
+const methodOverride = require('method-override');
+const bodyParser = require('body-parser');
+// const expressLayouts = require('express-ejs-layouts');
+// const expressEjsLayouts = require('express   -ejs-layouts');
+
+const app = express();
+
+
+// Middleware to parse URL-encoded bodies (form data)
+app.use(bodyParser.urlencoded({ extended: true }));
+// Middleware to parse JSON bodies
+app.use(bodyParser.json());
+// app.use(expressEjsLayouts);
+
+
+// Set view engine and views directory
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.engine('ejs', ejsMate);
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, './public')));
+app.use(methodOverride('_method'));
+
+
+app.get('/kys', (req, res) => {
+    res.render('routes/index.ejs');
+});
+app.get('/kys2', (req, res) => {
+    res.render('routes/index2.ejs');
+})
+
+
+app.listen(3000, () => console.log('listening on 3000'));
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./views/**/*.{ejs,html,css,js}",
+    'node_modules/preline/dist/*.js',
+  ],
+  darkMode: ['selector', '[data-mode="dark"]'],
+
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('preline/plugin'),
+  ],
+}
+
