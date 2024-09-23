fcc3f05bf25c4006aa287051cbc071c5aeff64bf
KYs <amitkys59@gmail.com>
2024-09-23 10:09:01 +0530

    switch to daisyUI

--- index.js
+++ index.js
@@ -1,70 +1,44 @@
 const express = require('express');
 const path = require('path');
-const ejsMate = require('ejs-mate');
-const methodOverride = require('method-override');
-const bodyParser = require('body-parser');
+const engine = require('ejs-mate');
 const mongoose = require('mongoose');
 const Notes = require('./db/db');
-
 const app = express();
 
-// .......................................................................
-// Middleware to parse URL-encoded bodies (form data)
-app.use(bodyParser.urlencoded({ extended: true }));
-// Middleware to parse JSON bodies
-app.use(bodyParser.json());
-
-// Set view engine and views directory
-app.set('view engine', 'ejs');
+// Use ejs-mate for all ejs templates:
+app.engine('ejs', engine);
 app.set('views', path.join(__dirname, 'views'));
-app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs'); // Set EJS as the view engine
+
 // Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, './public')));
-app.use(methodOverride('_method'));
-// ........................................................................
+app.use(express.static(path.join(__dirname, 'public')));
 
 mongoose.connect('mongodb://localhost:27017/note')
 .then(() => {console.log('connected to db')}).catch((e) => console.log('error to connect db', e));
 
-app.get('/', async(req, res) => {
+
+app.get("/", async(req, res) => {
     const all_note = await Notes.find({});
-    // console.log(all_note);
     res.render('routes/index.ejs', {all_note});
+});
+
+app.post('/note', async(req, res) => {
+    const newNote = {title: req.body.title, note: req.body.note};
+    const createdNote = await Notes.create(newNote);
+    // console.log(createdNote);
+    res.json({msg: 'notes created', noteId: createdNote._id});
+})
+app.put('/note/:id', async(req, res) => {
+    const id = req.params.id;
+    const newNote = {title: req.body.title, note: req.body.note};
+    const updatedNote = await Notes.findByIdAndUpdate(id, newNote);
+    res.json({msg: 'Note Updated'});
 })
 
-
-
-app.get('/:id', async(req, res) => {
-    const id = req.params.id; 
-    const data = await Notes.findById(id);
-    res.render('routes/singleNote.ejs', {data});
+app.delete("/note/:id", async(req, res) => {
+    const id = req.params.id;
+    const deletedNote = await Notes.findByIdAndDelete(id);
+    res.json({msg: 'notes delete'});
     
 })
-
-
-// app.post('/note', async(req, res) => {
-//     const newNote = {title: req.body.title, note: req.body.note};
-//     const createdNote = await Notes.create(newNote);
-//     // console.log(createdNote);
-//     res.json({msg: 'notes created', noteId: createdNote._id});
-// })
-// app.get('/note/:id', async(req, res) => {
-//     res.render('/routes/singleNote.ejs')
-// })
-// app.put('/note/:id', async(req, res) => {
-//     const id = req.params.id;
-//     const newNote = {title: req.body.title, note: req.body.note};
-//     const updatedNote = await Notes.findByIdAndUpdate(id, newNote);
-//     res.json({msg: 'Note Updated'});
-// })
-
-// app.delete("/note/:id", async(req, res) => {
-//     const id = req.params.id;
-//     const deletedNote = await Notes.findByIdAndDelete(id);
-//     res.json({msg: 'notes delete'});
-
-// })
-
-
-
 app.listen(3000, () => console.log('listening on 3000'));
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./views/***/**/*.{ejs,html,css,js}",
-    'node_modules/preline/dist/*.js',
   ],
   darkMode: ['selector', '[data-mode="dark"]'],
+  daisyui: {
+    themes: ["dark"],
+  },
 
   theme: {
     extend: {},
   },
   plugins: [
-    require('preline/plugin'),
+    require('daisyui'),
   ],
 }
 
